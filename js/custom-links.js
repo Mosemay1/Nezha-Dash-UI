f0ae2ff128d1b9fc5f63dd7ec8744af2bfbc88c9
Mosemay1 <mosemay70@gmail.com>
2025-11-02 12:49:20 +0800

    Update custom links and icons configuration

--- js/custom-links.js
+++ js/custom-links.js
@@ -7,15 +7,15 @@
 
 // ------------------ 自定义链接配置 ------------------
 window.CustomLinks = JSON.stringify([
-  { link: "https://blog.faiz.hidns.co", name: "Blog", icon: "icon-book" },
+  { link: "https://soso.tanzhan.qzz.io", name: "首页", icon: "icon-book" },
   {
-    link: "https://t.me/Kaixa913_bot",
+    link: "https://t.me/NPC_Cloud9_bot",
     name: "Telegram",
     icon: "icon-paper-plane",
   },
   {
-    link: "https://uptime.faiz.us.kg",
-    name: "Uptime",
+    link: "https://npccloud.xyz/",
+    name: "友情链接",
     icon: "icon-hourglass-start",
   },
 ]); // 导航栏的自定义链接 (JSON格式), 新增 icon 字段，请确保 icon值为 iconfont.css 中真实存在的 class
@@ -83,3 +83,4 @@ if (document.readyState === 'loading') {
 } else {
   initCustomLinks();
 }
+
